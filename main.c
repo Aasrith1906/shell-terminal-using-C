59c3e177fee16693e3bdc43f5b69a472131cf616
TheSolverGroup <venkat.chenna31@gmail.com>
2020-01-11 15:17:55 +0530

    launching programs

--- main.c
+++ main.c
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 #include <conio.h>
 #include <string.h>
-
+#include <windows.h>
+#include <tchar.h>
 
 #ifndef buffer_size
     #define buffer_size 1024
@@ -74,6 +75,49 @@ char *shell_read_line(void)
     }
 }
 
+
+int shell_launch(char **args)
+{
+    STARTUPINFO si; //struct for startup info
+
+    PROCESS_INFORMATION pi; //struct for process info 
+
+    ZeroMemory(&si , sizeof(si));
+    si.cb = sizeof(si);
+    
+    ZeroMemory(&pi , sizeof(pi));
+
+    if(
+        !CreateProcess(
+            NULL ,
+            args[0],
+            NULL,
+            NULL,
+            FALSE,
+            0,
+            NULL,
+            NULL,
+            &si,
+            &pi
+
+        )
+    )
+    {
+        printf("Create process failed \n");
+        exit(EXIT_FAILURE);
+    }
+
+    WaitForSingleObject(pi.hProcess,INFINITE); //wait for child process to exit
+
+    CloseHandle(pi.hProcess);
+
+    CloseHandle(pi.hThread);
+
+    return 1;
+
+
+}
+
 char **shell_split_line(char *line)
 {   
     int buff_size = buffer_size;
@@ -96,7 +140,7 @@ char **shell_split_line(char *line)
 
         position++;
 
-        //printf("%s \n" , token);
+        printf("%s \n" , token);
 
         if(position >= buff_size )
         {
@@ -125,7 +169,9 @@ char **shell_split_line(char *line)
 
 int shell_execute(char **args)
 {
-    printf("executing \n");
+    printf("launching : %s" , args[0]);
+
+    shell_launch(args);
 
     return 1;
 }
